6d8f26264a43b920f832f8ac64eec90bf61f9310
Brian Lachance <blachance@gmail.comx>
2013-08-21 01:25:26 -0700

    password: Listen for form's submit event instead of button click
    This requires changing the "button" type to submit, since
    type="button" won't submit a form.

--- examplePages/password.js
+++ examplePages/password.js
@@ -1,6 +1,6 @@
 // Trusted code created by the website.
-var b = document.getElementById('passBut');
-b.addEventListener('click', function() {
+document.getElementsByTagName('form')[0].addEventListener('submit', function(e) {
+    e.preventDefault(); e.stopPropagation();
     var p = document.getElementById('pass');
     var pwd = p.value || 'default';
     var hash = hex_md5(pwd);
